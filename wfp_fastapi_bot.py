19ec0665adedbc9c025aa56c47390d2ef3398836
NephrologBOT <umanets.vet@gmail.com>
2025-05-02 09:50:20 +0300

    wfp_fastapi_bot.py

--- wfp_fastapi_bot.py
+++ wfp_fastapi_bot.py
@@ -139,8 +139,6 @@ async def start_handler(message: types.Message):
     )
 
 # --- Запуск бота ---
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
 @app.on_event("startup")
 async def on_startup():
     loop = asyncio.get_event_loop()
