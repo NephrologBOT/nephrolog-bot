65ea7e9bf55ad4033570e3947bb944281050875b
NephrologBOT <umanets.vet@gmail.com>
2025-05-02 23:27:38 +0300

    Update wfp_fastapi_bot.py

--- wfp_fastapi_bot.py
+++ wfp_fastapi_bot.py
@@ -131,6 +131,6 @@ async def on_shutdown():
 @app.post("/webhook")
 async def telegram_webhook(update: dict):
     telegram_update = Update.model_validate(update)
-    await app.state.dp.feed_update(bot, telegram_update, bot.loop)
+    await app.state.dp.feed_update(bot, telegram_update)
     return {"ok": True}
 
