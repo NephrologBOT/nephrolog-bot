14d0ed9f86db674fe9fd629cdbcc061010f4d244
NephrologBOT <umanets.vet@gmail.com>
2025-05-03 02:00:47 +0300

    Update wfp_fastapi_bot.py

--- wfp_fastapi_bot.py
+++ wfp_fastapi_bot.py
@@ -76,7 +76,7 @@ def create_invoice(uid: str, amount: str) -> str:
     data["currency"],
     *data["productName"],
     *map(lambda x: str(int(x)), data["productCount"]),
-    *map(lambda x: f"{x:.2f}", data["productPrice"])
+    *map(lambda x: str(int(x)) if float(x).is_integer() else f"{x:.2f}", data["productPrice"]),
 ]
     signature_string = ";".join(signature_parts)
     print("SIGNATURE_STRING:", signature_string)
