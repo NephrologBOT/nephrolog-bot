13c0b23545a8307c8c0c6a726bad74caf6e6518c
NephrologBOT <umanets.vet@gmail.com>
2025-05-02 13:05:29 +0300

    wfp_fastapi_bot.py

--- wfp_fastapi_bot.py
+++ wfp_fastapi_bot.py
@@ -62,7 +62,7 @@ async def pay_form(uid: str, amount: str = PRICE_UAH):
         "productPrice": [int(product_price)],
         "productCount": [int(product_count)],
         "clientFirstName": "User",
-        "clientLastName": uid,
+        "clientLastName": str(uid),
         "clientEmail": f"user{uid}@nephrolog.com",
         "serviceUrl": "https://nephrolog-bot.onrender.com/wfp-callback",
     }
